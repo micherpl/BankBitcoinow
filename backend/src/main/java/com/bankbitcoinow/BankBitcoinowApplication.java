2127e4aba5bf4880b42c0e02a79e108cd808c2b3
pkoziol <pkoziol@student.agh.edu.pl>
2017-05-09 18:26:19 +0200

    Disable security for all URLs.

--- backend/src/main/java/com/bankbitcoinow/BankBitcoinowApplication.java
+++ backend/src/main/java/com/bankbitcoinow/BankBitcoinowApplication.java
@@ -2,11 +2,25 @@ package com.bankbitcoinow;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
 public class BankBitcoinowApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BankBitcoinowApplication.class, args);
+	}
+
+	@Configuration
+	@EnableWebSecurity
+	public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.authorizeRequests()
+					.anyRequest().permitAll();
+		}
 	}
 }
