f9a4685c08e81d22168267db53b36fbc2c55cd1c
pkoziol <pkoziol@student.agh.edu.pl>
2017-05-09 19:11:04 +0200

    Fix Transaction entity mapping.

--- backend/src/main/java/com/bankbitcoinow/models/Transaction.java
+++ backend/src/main/java/com/bankbitcoinow/models/Transaction.java
@@ -5,9 +5,11 @@ import org.bitcoinj.core.Coin;
 import javax.persistence.Convert;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 import javax.persistence.Transient;
 import java.math.BigDecimal;
@@ -28,7 +30,8 @@ public class Transaction {
     private Address address;
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "transaction_seq_gen")
+    @SequenceGenerator(name = "transaction_seq_gen", sequenceName = "transaction_id_seq")
     public Long getId() {
         return id;
     }
