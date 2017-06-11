bd59cb89cc9fdee82d3a36413e230843311e44c3
pkoziol <pkoziol@student.agh.edu.pl>
2017-06-11 14:06:21 +0200

    Allow CORS requests from all origins.

--- backend/src/main/java/com/bankbitcoinow/BankBitcoinowApplication.java
+++ backend/src/main/java/com/bankbitcoinow/BankBitcoinowApplication.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class BankBitcoinowApplication {
@@ -21,6 +23,14 @@ public class BankBitcoinowApplication {
 		protected void configure(HttpSecurity http) throws Exception {
 			http.authorizeRequests()
 					.anyRequest().permitAll();
+		}
+	}
+
+	@Configuration
+	public class MvcConfig extends WebMvcConfigurerAdapter {
+		@Override
+		public void addCorsMappings(CorsRegistry registry) {
+			registry.addMapping("/**").allowedOrigins("*");
 		}
 	}
 }
